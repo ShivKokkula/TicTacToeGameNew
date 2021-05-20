174b672fbb21ab69b66acaa8ff1135dff0624a17
ShivKokkula <shivbk987@gmail.com>
2021-05-20 22:24:56 +0530

    Add - created board array in UC-1-Board-Array branch

--- TicTacToeGameNew.java
+++ TicTacToeGameNew.java
@@ -3,9 +3,22 @@ package TicTacToeGameNew;
 public class TicTacToeGameNew {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to the Tic-Tac-Toe Game");
 
+		System.out.println("Welcome to Tic-Tac-Toe Game !!!");
+		
+		board();
+		getUserChoice();
+
+	}
+	
+	public static void board() {
+		
+		char board[] = new char[10];
+		
+		for(int i=0; i<=10; i++) {
+			board[i] = ' ';
+	     }
+		
 	}
 
 }
